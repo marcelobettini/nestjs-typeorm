f779b9a6f3fa4af755ea0e05e70bd729fffdbc40
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2024-06-30 22:09:03 -0300

    connection to db now depends on datasource/typeorm.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
+import { TypeOrmModule } from './datasource/typeorm.module';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionsFilter } from './all-exceptions/all-exceptions.filter';
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'nest_mysql_typeorm_DB',
-      entities: [User],
-    }),
-  ],
+  imports: [UsersModule, TypeOrmModule],
   controllers: [AppController],
   providers: [
     AppService,
---
+++ src/datasource/typeorm.module.ts
@@ -0,0 +1,36 @@
+import { DataSource } from 'typeorm';
+import { Global, Module } from '@nestjs/common';
+
+@Global() // makes the module available globally for other modules once imported in the app modules
+@Module({
+  imports: [],
+  providers: [
+    {
+      provide: DataSource, // add the datasource as a provider
+      inject: [],
+      useFactory: async () => {
+        // using the factory function to create the datasource instance
+        try {
+          const dataSource = new DataSource({
+            type: 'mariadb',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'nest_mysql_typeorm_DB',
+            synchronize: false,
+            entities: [`${__dirname}/../**/**.entity{.ts,.js}`], // this will automatically load all entity file in the src folder
+          });
+          await dataSource.initialize(); // initialize the data source
+          console.log('Database connected successfully');
+          return dataSource;
+        } catch (error) {
+          console.log('Error connecting to database');
+          throw error;
+        }
+      },
+    },
+  ],
+  exports: [DataSource],
+})
+export class TypeOrmModule {}
